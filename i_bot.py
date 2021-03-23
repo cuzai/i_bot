48cd2db04f8811b69836b2cac1fbb99873067a06
cuzai <cuzai@naver.com>
2021-03-23 18:11:45 +0900

    210323_1811

---
+++ i_bot.py
@@ -0,0 +1,13 @@
+from Check_Chromedriver import Check_Chromedriver
+import numpy as np
+import selenium
+
+class I_Bot() :
+    def __init__(self) :
+        Check_Chromedriver.main()
+
+    def main(self) :
+        pass
+
+if __name__ == "__main__" :
+    i_bot = I_Bot()
